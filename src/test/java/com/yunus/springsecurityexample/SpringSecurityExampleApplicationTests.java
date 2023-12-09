2ef296ff0e693eeebeab3438436904066ba2027d
Yunus Emre Kaymak <y.e.imp@hotmail.com>
2023-12-10 01:00:58 +0300

    Project Initialized

---
+++ src/test/java/com/yunus/springsecurityexample/SpringSecurityExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunus.springsecurityexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
