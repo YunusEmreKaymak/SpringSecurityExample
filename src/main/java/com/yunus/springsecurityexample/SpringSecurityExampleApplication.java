cab9aa152172ef07b6937e4142b952225ecd186a
Yunus Emre Kaymak <y.e.imp@hotmail.com>
2023-12-14 23:53:04 +0300

    Dummy data saved using commandLineRunner

--- src/main/java/com/yunus/springsecurityexample/SpringSecurityExampleApplication.java
+++ src/main/java/com/yunus/springsecurityexample/SpringSecurityExampleApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
 import java.util.Set;
@@ -16,17 +17,17 @@ import java.util.Set;
 @SpringBootApplication
 public class SpringSecurityExampleApplication {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         SpringApplication.run(SpringSecurityExampleApplication.class, args);
     }
 
     @Bean
-    CommandLineRunner commandLineRunner(UserRepository userRepo, ProductRepository productRepo) {
+    CommandLineRunner commandLineRunner(UserRepository userRepo, ProductRepository productRepo, BCryptPasswordEncoder encoder) {
         return args -> {
-            userRepo.save(new User("user1","useraccount1","password1",true,true,true,true, Set.of(Role.ROLE_USER)));
-            userRepo.save(new User("user2","useraccount2","password2",true,true,true,true, Set.of(Role.ROLE_DEV)));
-            productRepo.save(new Product("product1", 3.2,"url1",4));
-            productRepo.save(new Product("product2", 3.5,"url2",7));
+            userRepo.save(new User("user1", "useraccount1", encoder.encode("password1"), true, true, true, true, Set.of(Role.ROLE_USER)));
+            userRepo.save(new User("user2", "useraccount2", encoder.encode("password2"), true, true, true, true, Set.of(Role.ROLE_DEV)));
+            productRepo.save(new Product("product1", 3.2, "url1", 4));
+            productRepo.save(new Product("product2", 3.5, "url2", 7));
         };
     }
 }
